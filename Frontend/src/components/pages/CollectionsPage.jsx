3edc5b9b994800a22af5bc3d018c9ac57741da30
JawadAhmad312 <jawadtanoli480@gmail.com>
2026-07-15 04:55:41 -0700

    Fix Collection import path

--- Frontend/src/components/pages/CollectionsPage.jsx
+++ Frontend/src/components/pages/CollectionsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import CollectionGrid from "../collection/CollectionGrid";
+import CollectionGrid from "../Collection/CollectionGrid";
 
 const CollectionsPage = () => {
 
